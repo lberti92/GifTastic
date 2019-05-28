9d59dd8346e3faa5c1837d1dda09b38312b82e18
Breeze <lberti@msn.com>
2019-05-27 23:16:01 -0600

    Add button working to add artist

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -10,89 +10,53 @@ function renderButtons() {
     });
 }
 
-
-$("button").on("click", function () {
-
-    // In this case, the "this" keyword refers to the button that was clicked
-    var person = $(this).attr("data-person"); // Michael J. Fox
-
-    // Consructing a URL to search Giphy for the name of the person who said the quote
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        person + "&api_key=nV8R8RZprkCia9P7rvv421Tc6FoiHOkq&limit=10";
-
-    // Performing our AJAX GET request
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-
-        // AFter the data come back from the API
-        .then(function (response) {
-
-            // Storing an array of results in the results variable
-            var results = response.data;
-
-            //Looping over every result item
-            for (var i = 0; i < results.length; i++) {
-
-    
-                var gifDiv = $("<div>");
-
-
-                var rating = results[i].rating;
-
- 
-                var p = $("<p>").text("Rating: " + rating);
-
-      
-                var personImage = $("<img>");
-
+function displayArtist (data) {
+    $("#add-artist").on("click", function (event) {
+        event.preventDefault();
         
-                personImage.attr("src", results[i].images.fixed_height.url);
+        var artist = $("#artist-input").val().trim();
+        artists.push(artist);
+        renderButtons();
+        $("#artist-input").val("");
+      });
 
-                gifDiv.prepend(p);
-                gifDiv.prepend(personImage);
+//     // In this case, the "this" keyword refers to the button that was clicked
+//     var person = $(this).attr("data-person"); // Michael J. Fox
 
-                   $("#movie-info").prepend(gifDiv);
-            }
-        });
-});
+//     // Consructing a URL to search Giphy for the name of the person who said the quote
+//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+//         person + "&api_key=nV8R8RZprkCia9P7rvv421Tc6FoiHOkq&limit=10";
 
-
-renderButtons();
-
-// function displayMovieInfo() {
-//     var movie = $(this).attr("data-name");
-//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + artist + "&limit=10&api_key=nV8R8RZprkCia9P7rvv421Tc6FoiHOkq";
-
+//     // Performing our AJAX GET request
 //     $.ajax({
-//         method: "GET",
-//         url: queryURL
+//         url: queryURL,
+//         method: "GET"
 //     }).then(function (response) {
 
-//         console.log(response)
+//         // Storing an array of results in the results variable
+//         var results = response.data;
 
-//     })
-// // }
-//     function renderButtons() {
-//         $("#movie-buttons").empty();
+//         //Looping over every result item
+//         for (var i = 0; i < results.length; i++) {
 
-//         for (var i = 0; movies.length; i++) {
-//             var button = $("<button>");
-//             button.addClass("movie-btn");
-//             button.attr("data-name", movies[i]);
-//             button.text(movies[i]);
-//             $("#movie-buttons").append(button);
-//         }
-//     }
-//     // $("#add-movie").on("click", function (event) {
-//     //     event.preventDefault();
-//     //     var movie = $("#movie-input").val().trim();
-//     //     movies.push(movie);
-//     //     // renderButtons();
-//     // });
+//             var gifDiv = $("<div>");
 
-//     // $(document).on("click", ".movie-btn");
+//             var rating = results[i].rating;
 
+//             var p = $("<p>").text("Rating: " + rating);
 
-// renderButtons();
+//             var personImage = $("<img>");
+
+//             personImage.attr("src", results[i].images.fixed_height.url);
+
+//             gifDiv.prepend(p);
+//             gifDiv.prepend(personImage);
+
+//             $("#movie-info").prepend(gifDiv);
+        
+        }
+//     });
+// });
+// }
+displayArtist();
+renderButtons();
