15f74d80b85fb3509005aa76cd611d8d98abc200
Breeze <lberti@msn.com>
2019-05-29 21:37:38 -0600

    Final sumbit of project

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -22,6 +22,7 @@ function displayArtist(data) {
 
     $("#artist-buttons").on("click", "button", function () {
         $("#artist-info").empty();
+
         var person = $(this).attr("data-person");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             person + "&api_key=nV8R8RZprkCia9P7rvv421Tc6FoiHOkq&limit=10";
@@ -35,45 +36,44 @@ function displayArtist(data) {
             var results = response.data;
 
             for (var i = 0; i < results.length; i++) {
-           
+
                 var gifDiv = $("<div>");
                 var rating = results[i].rating;
                 var p = $("<p>").text("Rating: " + rating);
-                var personImage = $("<img>");
-                personImage.attr("src", results[i].images.fixed_height.url);
+                var personImage = $("<img class='person'>");
+                personImage.attr("src", results[i].images.fixed_height_still.url);
+                personImage.attr("data-still", results[i].images.fixed_height_still.url);
+                personImage.attr("data-animate", results[i].images.fixed_height.url);
+                personImage.attr("data-state", "still");
                 gifDiv.append(p, personImage);
                 $("#artist-info").append(gifDiv);
+
             }
         })
 
 
     })
-   }
+}
 displayArtist();
 
-// $("#artist-buttons").on("click", function() {
+$("#artist-info").on("click", ".person", function () {
+
+    
+    var state = $(this).attr("data-state");
+    var stillURL = $(this).attr("data-still");
+    var animURL = $(this).attr("data-animate");
 
 
-//     button.attr("data-state, data-still, data-animate", artist);
-//     var state = $(this).attr("data-state"); 
- 
-//     var stillURL = $(this).attr("data-still"); 
-//     var animURL = $(this).attr("data-animate");
+    if (state === "still") {
+        $(this).attr({
+            "src": animURL,
+            "data-state": "animate"
+        });
 
-
-//     if (state === "still") {
-//       // if it is currently paused
-//       $(this).attr({
-//         "src": animURL,
-//         "data-state": "animate"
-//       });
-
-//     }else {
-//       // if it is currently animated
-//       $(this).attr ({
-//         "src": stillURL,
-//         "data-state": "still"
-//       });
-//     }
-
-//   });
+    } else {
+        $(this).attr({
+            "src": stillURL,
+            "data-state": "still"
+        });
+    }
+});
